f265783f936f1af513913cc670cfa4f29fca0273
Johanna Raisa <johanna.raisa@gmail.com>
2022-05-16 08:47:22 +0300

    Add path fix

--- set-development.sh
+++ set-development.sh
@@ -8,8 +8,8 @@ rm $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/SMSSendLinkMobilityDriver.pm
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/SsnProvider" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/SMSSendLinkMobilityDriver
-ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/SsnProvider.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/SMSSendLinkMobilityDriver.pm
+ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/SMSSendLinkMobilityDriver" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/SMSSendLinkMobilityDriver
+ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/SMSSendLinkMobilityDriver.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/SMSSendLinkMobilityDriver.pm
 
 perl $kohadir/misc/devel/install_plugins.pl
 
